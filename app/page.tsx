1af8ffdc5a866b9d8eb1878d534653e0b24d245c
mizomico <aroky.me@gmail.com>
2026-02-18 00:55:20 +0200

    Navbar Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/sections/Navbar";
 
 export default function Home() {
   return (
     <main className="ralative w-screen min-h-screen overflow-x-auto">
       <Navbar />
+      <section id="home" className="min-h-screen"></section>
+      <section id="services" className="min-h-screen bg-amber-200"></section>
+      <section id="about" className="min-h-screen bg-SageGray"></section>
+      <section id="work" className="min-h-screen bg-amber-500"></section>
     </main>
   );
 }
