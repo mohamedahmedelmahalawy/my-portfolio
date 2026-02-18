b0a29c41a7e0f1a8187ed1f57a1170144ef1123c
mizomico <aroky.me@gmail.com>
2026-02-18 06:58:48 +0200

    navbar showing at the beginning of page render fixed

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -22,11 +22,12 @@ function Navbar() {
     });
     gsap.set([linksRef.current, contactRef.current], {
       autoAlpha: 0,
-      x: -20,
+      x: -40,
     });
     tl.current = gsap
       .timeline({ paused: true })
       .to(navRef.current, {
+        autoAlpha: 100,
         xPercent: 0,
         duration: 1,
         ease: "power3.out",
@@ -124,7 +125,7 @@ function Navbar() {
     <>
       <nav
         ref={navRef}
-        className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
+        className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2 invisible"
       >
         <div className="flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl">
           {["Home", "Services", "About", "Work", "Contact"].map(
